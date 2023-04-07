2ee376abac4fc9ae09ed09fe3194f191cb6974ec
facu8990 <facu.vdp@gmail.com>
2023-04-07 17:34:06 -0300

    eslint

--- src/Login.jsx
+++ src/Login.jsx
@@ -5,27 +5,27 @@ import PocketBase from 'pocketbase';
 const pb = new PocketBase('http://192.168.1.141:7790');
 
 async function getLoginToken(userName, password) {
-    const authData = await pb.admins.authWithPassword(userName, password); 
-    //localStorage.setItem('authToken', authData);
-    //setAuthToken(authToken());
-    console.log(authData);
-    return authData;
+  const authData = await pb.admins.authWithPassword(userName, password); 
+  //localStorage.setItem('authToken', authData);
+  //setAuthToken(authToken());
+  console.log(authData);
+  return authData;
 }
 
 function LoginForm() {
-    const [email, setEmail] = createSignal('');
-    const [password, setPassword] = createSignal('');
+  const [email, setEmail] = createSignal('');
+  const [password, setPassword] = createSignal('');
 
-    return (
-        <div class='container'>
-            <article>
-                <h2>Login</h2>
-                <input type="email" placeholder="Email" value={email()} onInput={(e) => setEmail(e.target.value)} />
-                <input type="password" placeholder="Password" value={password()} onInput={(e) => setPassword(e.target.value)} />
-                <button onClick={() => getLoginToken(email(),password())}>Log in</button>
-            </article>
-        </div>
-    );
+  return (
+    <div class='container'>
+      <article>
+        <h2>Login</h2>
+        <input type="email" placeholder="Email" value={email()} onInput={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder="Password" value={password()} onInput={(e) => setPassword(e.target.value)} />
+        <button onClick={() => getLoginToken(email(),password())}>Log in</button>
+      </article>
+    </div>
+  );
 }
 
 export default LoginForm;
