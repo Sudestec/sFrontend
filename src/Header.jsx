9298edb195a5232884a20388d474f611d7c8c133
facu8990 <facu.vdp@gmail.com>
2023-04-27 07:48:58 -0300

    2

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,23 +1,28 @@
-import { usePocket } from './AuthContext';
+import { createEffect } from 'solid-js';
+import { usePocket, PocketProvider } from './AuthContext';
+// import { lazy } from 'solid-js';
 import capitalizeFirstLetter from './modules/capitalizeFirstLetter';
 import getLocalToken from './modules/getLocalToken';
+// const usePocket = lazy(() => import('./AuthContext'));
 
 function Welcome() {
   
-  const {record} = getLocalToken();
+  // const {record} = getLocalToken();
   const {login} = usePocket;
+
+  createEffect(() => console.log(login.data));
 
   return (
     <header>
       <div class="grid">
         <hgroup>
-          <h1>{record.username}</h1>
+          {/* <h1>{login.data.username}</h1> */}
           <ul>
-            <li>{record.email}</li>
-            <li>{record.type}</li>
+            {/* <li>{login.data.email}</li> */}
+            {/* <li>{login.data.type}</li> */}
           </ul>
         </hgroup>
-        <p>{login.data}</p>
+        {/* <p>{login.data}</p> */}
       </div>
     </header>
   );
@@ -25,6 +30,9 @@ function Welcome() {
 
 export default function Header() {
   return (
-    <Welcome/>
+    <PocketProvider>
+
+      <Welcome/>
+    </PocketProvider>
   );
 }
