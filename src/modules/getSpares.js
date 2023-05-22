b8dc3707ab4c9dc11eff92cf81d25716fc0d0666
sudestec <facu.vdp@gmail.com>
2023-05-21 22:21:39 -0300

    prolly gona delete

--- src/modules/getSpares.js
+++ src/modules/getSpares.js
@@ -1,13 +1,7 @@
-const getSpares = async (api, token, parameters = { page: '', perPage: '', sort: '', filter: '', expand: '' }) => {
-  const url = api+'/api/collections/spares',
+const getSpares = async (api, token, parameters) => {
+  const url = `${api}/api/collections/spares/records?page=${page}&perPage=${perPage}&sort=${'-updated'+sort}&filter=${filter}`,
     myHeaders = new Headers();
-  myHeaders.append('Authorization', `Bearer ${token}`);
-
-  const params = new Request(api, {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-  });
+  myHeaders.append('Authorization', token);
 
   const requestOptions = {
       method: 'GET',
@@ -15,7 +9,7 @@ const getSpares = async (api, token, parameters = { page: '', perPage: '', sort:
       redirect: 'follow'
     },
     { page, perPage, sort, filter, expand } = parameters,
-    data = await fetch(`${url}/records?page=${page}&perPage=${perPage}&sort=${'-updated'+sort}&filter=${filter}`, requestOptions),
+    data = await fetch(url, requestOptions),
     jsonData = await data.json(); 
   if (data.status !== 200) {
     return data.status;
